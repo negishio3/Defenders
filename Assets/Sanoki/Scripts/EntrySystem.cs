b048998f1a7eaab5da75af9973c3718cc8c3c043
sanoki <nkc.hirokazu.sanoki@gmail.com>
2019-02-15 15:04:01 +0900

    エントリーが多分完成？ バグあるかも

---
+++ Assets/Sanoki/Scripts/EntrySystem.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class EntrySystem : SingletonMonoBehaviour<EntrySystem>
+{
+    public enum PLAYERNUM
+    {
+        ONE,
+        TWO,
+        THREE,
+        FOUR
+    }
+
+    public static int[] playerNumber = { -1, -1, -1, -1 };
+
+    int playerCount = 0;
+
+    float[] rotations = { 0, 0, 0, 0 };
+
+    SceneFader sceneFader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneFader = FindObjectOfType<SceneFader>();
+        for(int i = 0; i < playerNumber.Length; i++)
+        {
+            playerNumber[i] = -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerCount < 5)
+        {
+            for(int i = 1; i < 5; i++)
+            {
+                if (GamePad.GetButtonDown(GamePad.Button.A, (GamePad.Index)i))
+                {
+                    AddPlayer((PLAYERNUM)i);
+                }
+            }
+        }
+        if (playerCount > 0)
+        {
+            for(int i = 1; i < 5; i++)
+            {
+                if (GamePad.GetButtonDown(GamePad.Button.B, (GamePad.Index)i))
+                {
+                    RemovePlayer((PLAYERNUM)i);
+                }
+
+                if (GamePad.GetButtonDown(GamePad.Button.Y, (GamePad.Index)i))
+                {
+                    GameStart((PLAYERNUM)i);
+                }
+            }
+        }
+    }
+
+    void AddPlayer(PLAYERNUM _player)
+    {
+        bool flg = false;
+
+        foreach(int _number in playerNumber)
+        {
+            if (_number == (int)_player)
+            {
+                flg = true;
+
+                Debug.Log("登録できません");
+            }
+        }
+
+        if (!flg)
+        {
+            playerNumber[playerCount] = (int)_player;
+
+            Debug.Log((PLAYERNUM)playerCount + " player 登録完了");
+
+            playerCount++;
+        }
+    }
+
+    void RemovePlayer(PLAYERNUM _player)
+    {
+        for(int i = 0; i < playerNumber.Length; i++)
+        {
+            if ((int)_player == playerNumber[i])
+            {
+                playerNumber[i] = -1;
+                playerCount--;
+            }
+        }
+
+        List<int> array = new List<int>();
+        int[] temp = { -1, -1, -1, -1 };
+
+        for(int i = 0; i < playerNumber.Length; i++)
+        {
+            if(playerNumber[i] != -1)
+            {
+                array.Add(playerNumber[i]);
+            }
+        }
+
+        for(int i = 0; i < array.Count; i++)
+        {
+            temp[i] = array[i];
+        }
+
+        for(int i = 0; i < temp.Length; i++)
+        {
+            playerNumber[i] = temp[i];
+            Debug.Log(playerNumber[i]);
+        }
+    }
+
+    void GameStart(PLAYERNUM _player)
+    {
+        foreach(int _number in playerNumber)
+        {
+            if (_number == (int)_player)
+            {
+                sceneFader.SceneChange("Template");
+            }
+        }
+    }
+
+}
--- Assets/Sanoki/Scripts/PlayerInput.cs
+++ Assets/Sanoki/Scripts/PlayerInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using GamepadInput;
 
-public class Player : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
     public GamePad.Index playerNo;// コントローラーの番号
     Vector3 player_Distans;// プレイヤーの進む方向
@@ -21,11 +21,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) pauseFlg = !pauseFlg;
+        if (GamePad.GetButtonDown(GamePad.Button.Start,playerNo)) pauseFlg = !pauseFlg;
         if (pauseFlg) return;
         StickInput();
     }
-
+    // スティックのインプット
     void StickInput()
     {
         var gamepadState = GamePad.GetState(playerNo, false);// コントローラーからの入力を受け取る
@@ -36,18 +36,38 @@ public class Player : MonoBehaviour
             player_Distans = new Vector3(LStickAxisX, 0, LStickAxisY);// 移動する方向を加算
         else player_Distans = new Vector3(0, 0, 0);// 入力がないなら移動しない
 
-        //if (LStickAxisX > 0 || LStickAxisX < 0 || LStickAxisY > 0 || LStickAxisY < 0)
-        //{
-        //    Vector3 direction = new Vector3(LStickAxisX, 0, LStickAxisY);
-        //    transform.rotation = Quaternion.LookRotation(direction);
-        //}
+        // スティックの入力を確認
+        if (LStickAxisX > 0 || LStickAxisX < 0 || LStickAxisY > 0 || LStickAxisY < 0)
+        {
+            Vector3 direction = new Vector3(LStickAxisX, 0, LStickAxisY);// スティックの入力
+            transform.rotation = Quaternion.LookRotation(direction);//回転
+        }
 
         player_Pos += player_Distans * Time.deltaTime * speed;// 移動
         transform.position = player_Pos;// 座標の更新
     }
-
+    // ボタンのインプット
     void ButtonInput()
     {
+        // ダッシュ？
+        if (GamePad.GetState(playerNo, false).A)
+        {
 
+        }
+        // 攻撃
+        if(GamePad.GetState(playerNo, false).B)
+        {
+
+        }
+        // 武器チェンジ？
+        if(GamePad.GetState(playerNo, false).X)
+        {
+
+        }
+        // アイテム使用？
+        if(GamePad.GetState(playerNo, false).Y)
+        {
+
+        }
     }
 }
