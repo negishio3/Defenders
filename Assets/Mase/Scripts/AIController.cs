7cb0a58dfc1382d07866f243bff8eef8d1ee3ae3
mase <nkc.yuya.mase.2016@gmail.com>
2019-02-18 14:40:57 +0900

    AI(谷川)を自動生成できるようにした

--- Assets/Mase/Scripts/AIController.cs
+++ Assets/Mase/Scripts/AIController.cs
@@ -13,6 +13,7 @@ public class AIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
--- Assets/Mase/Scripts/AIGenerator.cs
+++ Assets/Mase/Scripts/AIGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AIGenerator : MonoBehaviour
 {
+    public GameObject obj;//生成するオブジェクト
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class AIGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(obj,(this.transform.position), Quaternion.identity);
+        }
     }
 }
