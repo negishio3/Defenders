e022ab095d2d36767c448b1c446ce261cf47d446
mase <nkc.yuya.mase.2016@gmail.com>
2019-03-11 12:37:42 +0900

    ただいまwave考え中

--- Assets/Mase/Scripts/AIGenerator.cs
+++ Assets/Mase/Scripts/AIGenerator.cs
@@ -6,12 +6,13 @@ public class AIGenerator : MonoBehaviour
 {
     public GameObject obj;//生成するオブジェクト
     private float lastAttackTime;
-    private float attackInterval = 2f;
+    private float attackInterval = 1f;
+    public float timeout;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(EnemySummons());
     }
 
     // Update is called once per frame
@@ -19,9 +20,16 @@ public class AIGenerator : MonoBehaviour
     {
         if (Time.time > lastAttackTime + attackInterval)
         {
-            Instantiate(obj,(this.transform.position), Quaternion.identity);
+            Instantiate(obj, (this.transform.position), Quaternion.identity);
 
             lastAttackTime = Time.time;
         }
+    }
+
+    //AI召喚
+    IEnumerator EnemySummons()
+    {
+
+        yield return new WaitForSeconds(timeout);
     }
 }
