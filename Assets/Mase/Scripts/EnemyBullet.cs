4be0adaff91c3a8fc7c2a9a8cc61b8cec1c2018b
mase <nkc.yuya.mase.2016@gmail.com>
2019-03-12 14:44:02 +0900

    AIのwave作成中

---
+++ Assets/Mase/Scripts/EnemyBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                PlayerInput pi = other.GetComponent<PlayerInput>();
+                break;
+        }
+    }
+}
